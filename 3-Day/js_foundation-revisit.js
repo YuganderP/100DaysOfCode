d48cc70c104e41c8c6fa7011049d2060c314b265
yugander <yuganderpilli@outlook.com>
2024-03-29 13:04:58 +0530

    day 3 commit

---
+++ 3-Day/js_foundation-revisit.js
@@ -0,0 +1,28 @@
+const fs = require("fs")
+function sample (){
+
+    return new Promise((resolve,reject)=>{
+        fs.readdir("./",(err,files)=>{
+            resolve(files)
+        })
+    })
+}
+
+
+function prints(data){
+    console.log(data)
+}
+
+
+// sample().then(prints)
+//  async await practice 
+let onDone=()=>{console.log("timeout complete")}
+
+async function printss(data){
+    await sample().then(prints)
+        console.log("File reading has been completed")
+ setTimeout(onDone,2000)
+    console.log("the last line in the async function")
+    }
+
+printss()
