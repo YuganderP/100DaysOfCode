329c3122120487590e15143aec5b32dd631eeb63
yugander <yuganderpilli@outlook.com>
2024-04-12 16:47:58 +0530

    frontend work started

---
+++ Projects/TodoProject/Frontend/todo-frontend/src/Components/CreateTodo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+function CreateTodo(){
+
+    return <div>
+        <input placeholder="title "type="text" id="title"></input>
+        <br />
+        <input placeholder="desciption "type="text" id ="desciption" />
+        <br />
+        <button>Add a todo</button>
+    </div>
+}
